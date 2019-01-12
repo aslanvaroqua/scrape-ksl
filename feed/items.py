c572a379c6f524d88cf30386d541e9627a97889c
Aslan Varoqua (Skylines Digital USA) <aslan.varoqua@gmail.com>
2019-01-11 17:21:26 -0700

    Update items.py

--- feed/items.py
+++ feed/items.py
@@ -9,20 +9,11 @@ import scrapy
 
 
 class FeedItem(scrapy.Item):
-    product_id = scrapy.Field()
-    store_id = scrapy.Field()
-    brand = scrapy.Field()
-    image_link = scrapy.Field()
-    title = scrapy.Field()
-    link = scrapy.Field()
-    price = scrapy.Field()
-    google_product_category = scrapy.Field()
-    color = scrapy.Field()
-    size = scrapy.Field()
-    description = scrapy.Field()
-    description_legacy = scrapy.Field()
-    gender = scrapy.Field()
+    cell_phone = scrapy.Field()
+    home_phone = scrapy.Field()
+    category = scrapy.Field()
+    sub_category = scrapy.Field()
 
 
-    product_id,store_id,brand,image_link,title,link,price,google_product_category,color,size,description,gender = scrapy.Field()
+    cell_phone,home_phone,category,sub_category = scrapy.Field()
 
